cfd2927c01fbf6cecd3c85b914f10a4bb4207cf7
Shane Francis <bigbeeshane@gmail.com>
2014-03-09 13:13:46 +0000

    amami: default to xhdpi

--- amami.mk
+++ amami.mk
@@ -28,8 +28,8 @@ PRODUCT_COPY_FILES += \
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Device specific init
 PRODUCT_COPY_FILES += \
