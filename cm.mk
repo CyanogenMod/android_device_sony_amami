0828f83ceba9a83802f1f6ac90502f5e03b26dd5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-03-13 22:52:09 +0100

    amami: misc update
    * update signature
    * fix usb id

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.3/14.2.A.1.114/jfv_jg:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.3 RHINE-1.1.1-131217-0933 22 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.3/14.2.A.1.142/cfv_jg:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.3 RHINE-1.1.1-140124-0137 36 test-keys"
 
 PRODUCT_NAME := cm_amami
 PRODUCT_DEVICE := amami
