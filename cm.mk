b78aa580a825cdce05ae21b7603ded84cecc95fd
OmarEinea <eineao@gmail.com>
2014-09-16 15:52:41 +0400

    amami: update signature (.133)
    Signed-off-by: Omar Einea <eineao@gmail.com>
    Change-Id: I342e16e983999d6be6c1a7f2ff88c2a6f120aec4

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.3/14.2.A.1.142/cfv_jg:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.3 RHINE-1.1.1-140124-0137 36 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.4.4/14.4.A.0.133/k___jQ:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.4.4 14.4.A.0.133 k___jQ release-keys"
 
 PRODUCT_NAME := cm_amami
 PRODUCT_DEVICE := amami
