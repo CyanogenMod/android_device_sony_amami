08b53ba3fc70dbda084a880616d8ca9e46455c8e
David Viteri <davidteri91@gmail.com>
2015-06-19 10:44:27 +0200

    amami: update fingerprint to latest lollipop release
    Change-Id: Icaa64022b293c6293134e5a2c2dc9e619c1008e9
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm.mk
+++ cm.mk
@@ -6,7 +6,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.4.4/14.4.A.0.133/k___jQ:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.4.4 14.4.A.0.133 k___jQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5503/D5503:5.0.2/14.5.A.0.270/3750474323:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5503-user 5.0.2 14.5.A.0.270 3750474323 release-keys"
 
 PRODUCT_NAME := cm_amami
 PRODUCT_DEVICE := amami
