b5a93eee7c0599e72de368d8ee235f372ec8d173
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-02-27 11:51:05 -0300

    amami: move touchscreen *.idc to device tree
    Change-Id: Ic64cb7bea6c8299fd6df1a26fe27bef6aa175fd0

--- amami.mk
+++ amami.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/clearpad.idc:system/usr/idc/clearpad.idc
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
